879f02a12990d7d67588656ee0365b9ad451ae92
dbogym <ko07073@gmail.com>
2025-03-20 17:34:40 +0900

    first commit

---
+++ src/main/java/io/dbogym/Main.java
@@ -0,0 +1,12 @@
+// 과제1 - 단계 1 - 1
+package io.dbogym;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("명령어 > ");
+        sc.
+    }
+}
