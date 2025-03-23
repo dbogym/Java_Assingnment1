fd0434d7564e6c0dddde8cb0ecb0a9f32d6380ca
dbogym <ko07073@gmail.com>
2025-03-24 01:26:10 +0900

    단계 1 - 3 (게시글 작성)

--- src/main/java/io/dbogym/Main.java
+++ src/main/java/io/dbogym/Main.java
@@ -1,4 +1,4 @@
-// 단계 1 - 2.b
+// 단계 1 - 3
 package io.dbogym;
 
 import java.util.Scanner;
@@ -6,15 +6,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean status = true;
+        int id= 0;
 
-        while (true ){
-            System.out.print("명령어 > ");
+        while ( status ){
+            System.out.print("명령어> ");
             String s = sc.nextLine().trim();
-            if(s.equals("종료") || s.equals("exit")) {
-                System.out.println("프로그램이 종료됩니다.");
-                break;
+
+            switch ( s ){
+                case "종료":
+                    System.out.println("프로그램이 종료됩니다.");
+                    status = false;
+                    break;
+
+                case "작성":
+                    System.out.println("게시글을 작성합니다.");
+
+                    System.out.print("제목: ");
+                    String title = sc.nextLine().trim();
+
+                    System.out.print("내용: ");
+                    String body = sc.nextLine().trim();
+
+                    id++;
+                    Post newPost = new Post(id, title, body);
+
+                    System.out.println("newPost = " + newPost);
+                    break;
+
+                default:
+                    System.out.println("존재하지 않는 명령어 입니다.");
+
             }
-            System.out.println("존재하지 않는 명령어 입니다.");
         }
     }
 }
---
+++ src/main/java/io/dbogym/Post.java
@@ -0,0 +1,68 @@
+package io.dbogym;
+
+import java.time.LocalDate;
+
+public class Post {
+    int id;
+
+    String title;
+    String body;
+
+    LocalDate createDate;
+    LocalDate updateDate;
+
+    public Post(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.createDate = LocalDate.now();
+        this.updateDate = LocalDate.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDate getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDate updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
+                '}';
+    }
+}
